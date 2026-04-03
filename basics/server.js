e947b7996473b3c3a9470f2d8fed3694c8247738
Shreyash <shreyashsabale2646@gmail.com>
2026-04-03 11:28:50 +0530

    Node.js: created form and sent HTML response using http module

--- basics/server.js
+++ basics/server.js
@@ -2,7 +2,24 @@ const http = require('http');
 
 // Creating a basic HTTP server
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    
+    const url=req.url;
+
+    if(url === "/"){
+        res.setHeader('Content-Type', 'text/html');
+
+        res.write('<html>');
+        res.write('<head><title>Enter the msg</title></head>');
+        res.write('<body>');
+         res.write('<form action="/message" method="POST">')
+        res.write('<input type="text" name="message">');
+        res.write('<button type="submit">Send</button>');
+        res.write('</form>');
+        res.write('</body>');
+        res.write('</html>');
+
+        return res.end();
+    }
 
     res.setHeader('Content-Type', 'text/html');
 
